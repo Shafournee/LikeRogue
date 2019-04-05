8b6c92a2a31433ffee8824a6dc0a7c55fa376b28
Shafournee <31718336+Shafournee@users.noreply.github.com>
2019-04-05 15:46:40 -0400

    Added player movement and shooting

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+
+    enum moveDir { up, down, left, right, none }
+
+    moveDir uddirection;
+
+    moveDir lrdirection;
+
+    float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        if (rigidBody == null)
+            Debug.Log("Player lacks a rigid body!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            uddirection = moveDir.up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            uddirection = moveDir.down;
+        }
+        else
+        {
+            uddirection = moveDir.none;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            lrdirection = moveDir.left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            lrdirection = moveDir.right;
+        }
+        else
+        {
+            lrdirection = moveDir.none;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (uddirection == moveDir.up)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.normalized.x, 1f) * speed;
+        }
+        else if (uddirection == moveDir.down)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.normalized.x, -1f) * speed;
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.normalized.x, 0f) * speed;
+        }
+
+        if (lrdirection == moveDir.right)
+        {
+            rigidBody.velocity = new Vector2(1f, rigidBody.velocity.normalized.y) * speed;
+        }
+        else if (lrdirection == moveDir.left)
+        {
+            rigidBody.velocity = new Vector2(-1f, rigidBody.velocity.normalized.y) * speed;
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0f, rigidBody.velocity.normalized.y) * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+
+    float bulletSpeed = 15f;
+
+    bool isShooting;
+
+    enum shootDir { up, down, left, right }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (bullet == null)
+            Debug.Log("No bullet attached!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isShooting)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                StartCoroutine(shootBullet(shootDir.up));
+            }
+            else if(Input.GetKey(KeyCode.DownArrow))
+            {
+                StartCoroutine(shootBullet(shootDir.down));
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                StartCoroutine(shootBullet(shootDir.left));
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                StartCoroutine(shootBullet(shootDir.right));
+            }
+        }
+    }
+
+    IEnumerator shootBullet(shootDir shootingDir)
+    {
+        isShooting = true;
+        yield return new WaitForFixedUpdate();
+
+        // Calculate the vector of the bullet based on the input direction
+        // And the position relative to the player
+        float spawnDisplacement = 1f;
+        Vector2 bulletSpawnPos = new Vector2();
+        Vector2 bulletVel = new Vector2();
+        switch (shootingDir)
+        {
+            case shootDir.up:
+                bulletVel = new Vector2(0f, 1f);
+                bulletSpawnPos = new Vector2(0f, spawnDisplacement);
+                break;
+            case shootDir.down:
+                bulletVel = new Vector2(0f, -1f);
+                bulletSpawnPos = new Vector2(0f, -spawnDisplacement);
+                break;
+            case shootDir.right:
+                bulletVel = new Vector2(1f, 0f);
+                bulletSpawnPos = new Vector2(spawnDisplacement, 0f);
+                break;
+            case shootDir.left:
+                bulletVel = new Vector2(-1f, 0f);
+                bulletSpawnPos = new Vector2(-spawnDisplacement, 0f);
+                break;
+        }
+
+        // TODO: add the players movement to modify the bullet speed
+
+        // Now let's instantiate the bullet
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        GameObject newBullet = Instantiate(bullet, pos + bulletSpawnPos, Quaternion.identity);
+        newBullet.GetComponent<Rigidbody2D>().velocity = bulletVel * bulletSpeed;
+
+
+        // Wait for the player to be able to shoot the next bullet
+        yield return new WaitForSeconds(.1f);
+        isShooting = false;
+    }
+}
